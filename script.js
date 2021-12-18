bf300ba9bba2c2ffb5a3469ab5aa8f842860ed7e
jjtheleg <joaonetotfm@gmail.com>
2021-12-18 18:09:48 -0300

    second project in js

---
+++ script.js
@@ -0,0 +1,14 @@
+function converter() {
+    var temp = document.getElementById('temp')
+    var tempCelsius = document.getElementById('celsius').value
+    var tempFahrenheit = (tempCelsius / 5) * 9 + 32
+    temp.value = tempFahrenheit
+}
+
+function verifyTemp() {
+    temp = document.getElementById('temp')
+    if (temp.value > 90) {
+        var comentary = document.getElementById('comentary')
+        comentary.innerHTML = 'eita calor da mizera'
+    }
+}
