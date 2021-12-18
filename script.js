b78791e0251ba2640fdbdd2d3ff81ddae541c7eb
jjtheleg <joaonetotfm@gmail.com>
2021-12-18 19:18:05 -0300

    feature

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ function verifyTemp() {
     if (temp.value > 90) {
         var comentary = document.getElementById('comentary')
         comentary.innerHTML = 'eita calor da mizera'
+    } else {
+        comentary.innerHTML = ''
     }
 }
